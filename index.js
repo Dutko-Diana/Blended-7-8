fc2272efc26eedf84456868a3f7a97dec6454f15
Diana Dutko <dutkodiana2007@gmail.com>
2024-11-09 15:26:55 +0200

    task1 and task2

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 //index
+
+
+// task-1
+
+// const btn = document.querySelector('button');
+// const input = document.querySelector('#input');
+
+// btn.addEventListener('click', () => {
+//     input.value = 'Bye!';
+// })
+
+// task-2
+
+// const btn = document.querySelector('button');
+// const input = document.querySelector('#input');
+
+// btn.addEventListener('click', handlerClick);
+
+// function handlerClick() {
+//     alert(input.value);
+// }
